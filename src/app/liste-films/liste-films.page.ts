f7d293898bf0692a04c915c69322bad643370d4d
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-09-17 11:19:49 +0200

    Ajout de l'attribut lesFilms dans la classe ListeFilmPage avec 3 films

--- src/app/liste-films/liste-films.page.ts
+++ src/app/liste-films/liste-films.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem } from '@ionic/angular';
+import { Film } from '../../modeles/Film' ;
 
 @Component({
   selector: 'app-liste-films',
@@ -9,11 +10,23 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem } from '@ionic/ang
   styleUrls: ['./liste-films.page.scss'],
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem]
 })
-export class ListeFilmsPage implements OnInit {
+export class ListeFilmsPage implements OnInit 
+{
+  protected lesFilms: Array<Film> ;
 
-  constructor() { }
+  constructor() 
+  { 
+    // On crée le tableau et on copie sa référence dans l'attribut lesFilms
+    this.lesFilms = new Array<Film>() ; 
 
-  ngOnInit() {
+    // On ajoute quelques films dans le tableau
+    this.lesFilms.push( new Film( 1, "Star Wars", 1976 ) ) ;
+    this.lesFilms.push( new Film( 2, "Avatar", 2006 ) ) ;
+    this.lesFilms.push( new Film( 3, "La folie des grandeurs", 1966 ) ) ;
+  }
+
+  ngOnInit() 
+  {
   }
 
 }
