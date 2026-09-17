0bddce379a12b08e0337ff1f223839af1c5dc5b0
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-09-17 15:53:14 +0200

    Abonnement au service FilmService pour récupérer les films en provenance de la BD et les afficher

--- src/app/liste-films/liste-films.page.ts
+++ src/app/liste-films/liste-films.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem } from '@ionic/angular';
 import { Film } from '../../modeles/Film' ;
+import { FilmService } from '../../services/film.service';
 
 @Component({
   selector: 'app-liste-films',
@@ -13,20 +14,22 @@ import { Film } from '../../modeles/Film' ;
 export class ListeFilmsPage implements OnInit 
 {
   protected lesFilms: Array<Film> ;
+  protected filmService: FilmService ;
 
-  constructor() 
+  constructor( filmService: FilmService ) 
   { 
+    this.filmService = filmService ;
+
     // On crée le tableau et on copie sa référence dans l'attribut lesFilms
     this.lesFilms = new Array<Film>() ; 
-
-    // On ajoute quelques films dans le tableau
-    this.lesFilms.push( new Film( 1, "Star Wars", 1976 ) ) ;
-    this.lesFilms.push( new Film( 2, "Avatar", 2006 ) ) ;
-    this.lesFilms.push( new Film( 3, "La folie des grandeurs", 1966 ) ) ;
   }
 
   ngOnInit() 
   {
+    this.filmService.getTousLesFilms().subscribe( (lesFilms)=>
+    {
+      this.lesFilms = lesFilms ;
+    }) ;
   }
 
 }
